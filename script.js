430157695e6c0f7fd2ef8d4701dbf3e9356c6991
Manchikanti Likitha <manchikantilikitha4@gmail.com>
2026-06-12 10:40:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Thank you for contacting Coffee Café! We will reach you soon.");
+
+});
